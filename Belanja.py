0e7803aa1bf88022e25a597e50c5c47e2ade9fbe
Purhita <purhito@gmail.com>
2025-05-05 16:22:19 +0900

    initial commit - belanja

---
+++ Belanja.py
@@ -0,0 +1,24 @@
+# Belanja dengan mengecek stok barang
+
+print('Ibu berkata kepada Budi: "Budi, pergi ke toko untuk membeli 1 botol susu" ')
+print('Budi berkata: "iya Bu"')
+print('Ibu berkata: "kalau ada telur, beli 6 butir"')
+print('Budi berkata: "iya Bu"')
+print('Budi pergi ke Toko')
+
+jumlah_susu = 30
+jumlah_telur = 30
+
+if jumlah_susu == 0:
+    print("Budi tidak membeli susu")
+else:
+    print("Budi membeli 1 botol susu")
+
+if jumlah_telur == 0:
+    print("Budi tidak membeli telur")
+else:
+    print("Budi membeli 6 butir telur")
+
+print("Budi pulang ke rumah")
+print("Budi melapor ke ibunya")
+print("Selesai")
