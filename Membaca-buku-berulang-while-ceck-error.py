1806af91c8e06a1e62c4e97d0170c6b8aae0059b
Purhita <purhito@gmail.com>
2025-05-06 10:43:01 +0900

    menambahkan jumlah pengulangan membaca

--- Membaca-buku-berulang-while-ceck-error.py
+++ Membaca-buku-berulang-while-ceck-error.py
@@ -1,11 +1,15 @@
 # membaca buku dengan pengulangan
 
-print('Ibu berkata: "Budi, ini ada buku, baca semua dan fahami"')
+jumlah_buku = 3
+
+print(f'Ibu berkata: "Budi, ini ada {jumlah_buku} buku, baca semua dan fahami"')
 print('Budi berkata: "baik Bu"')
 
+
 def baca_buku():
-    total_buku = 3
+    total_buku = jumlah_buku
     buku_saat_ini = 1
+    jumlah_baca = 0
 
     while buku_saat_ini <= total_buku:
         print(f"\nBudi sedang membaca buku ke-{buku_saat_ini}...")
@@ -15,13 +19,15 @@ def baca_buku():
                 if buku_saat_ini < total_buku:
                     print(f"Bagus, lanjut ke buku ke-{buku_saat_ini + 1}.")
                 buku_saat_ini += 1
+                jumlah_baca += 1
                 break
             elif jawab == "tidak":
                 print("\nUlangi membaca buku yang sama sampai faham.")
+                jumlah_baca += 1
             else:
                 print("Input tidak dikenali, harap jawab 'ya' atau 'tidak'.")
-
-    print("\nSelamat! Budi sudah memahami semua buku yang harus dibaca.")
+    print(f"\nBudi sudah membaca buku sebanyak {jumlah_baca} x")
+    print(f"Selamat Budi, kamu sudah memahami semua {total_buku} buku yang harus dibaca.")
 
 
 if __name__ == "__main__":
