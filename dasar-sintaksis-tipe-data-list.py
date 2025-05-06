6aed36721d00ad2f7ca331211695097c2fd7aa69
Purhita <purhito@gmail.com>
2025-05-06 16:23:06 +0900

    dasar-sintaksis-tipe-data-list2

--- dasar-sintaksis-tipe-data-list.py
+++ dasar-sintaksis-tipe-data-list.py
@@ -29,3 +29,33 @@ print('Tambahkan data dalam daftar_buku')
 daftar_buku.append('Dunia anak')
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print('\nclear list')
+daftar_buku.clear()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nGanti elemen Kedua')
+daftar_buku = ['Harry Potter', 'Lima Sekawan', 'Narnia','4WD']
+daftar_buku[1] = 'Enam Sekawan'
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nAmbil elemen ke-2')
+buku = daftar_buku.pop(1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nBuku yang diambil tadi')
+print(buku)
+
+print('\nPop')
+daftar_buku.pop()   #pop() di ambil paling ujung terakhir
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPop -2')
+daftar_buku = ['Harry Potter', 'Lima Sekawan', 'Narnia','4WD']
+daftar_buku.pop(-2)   #pop() di ambil dari paling ujung terakhir hitung ke kiri
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
